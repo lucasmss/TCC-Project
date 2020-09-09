ac255fea08dc48cce0c85a300a54f57b39685414
lucasmss <lucasmss16@gmail.com>
2020-09-08 23:59:03 -0300

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root to: "static_pages#index"
+
+  root            to: 'static_pages#index'
+  get 'sobre',    to: 'static_pages#sobre'
+  get 'contato',  to: 'static_pages#contato'
+
+  get 'cadastro', to: 'users#new'
+  get 'entrar',   to: 'sessions#new'
+  post 'entrar',  to: 'sessions#create'
+  delete 'sair',  to: 'sessions#destroy'
+
+  resources :users, only: [:show, :new, :create, :edit, :update] do
+    resources :contacts, only: [:index, :new, :create, :edit, :update, :destroy]
+  end
+  resources :sessions, only: [:new, :create, :destroy]
+
 end
